e91b972876fe3e810a38edf94f0c483143fb6722
grilosystems <mangopu@gmail.com>
2021-08-12 14:44:47 -0500

    Original source

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // A quien saludar como nombre la entrada 
-    const nameToSaludo = core.getInput('quien-saluda');
-    console.log(`Hola ${nameToSaludo}!`);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`El evento payload: ${payload}`);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
